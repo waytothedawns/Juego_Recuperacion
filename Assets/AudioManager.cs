f050097b55492db177085cef858f263dcec26dce
waytothedawns <55994075+waytothedawns@users.noreply.github.com>
2020-09-02 17:43:55 +0200

    menu opciones

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] Slider sliderVolume;
+    [SerializeField] AudioMixer audioMixer;
+    public void SetVolume()
+    {
+        audioMixer.SetFloat("mainVolume", sliderVolume.value);
+        
+    }
+}
---
+++ Assets/VideoManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoManager : MonoBehaviour
+{
+    public void SetFullDisplay(bool b)
+    {
+        Screen.fullScreen = b;
+    }
+}
